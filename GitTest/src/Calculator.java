d21be4cc408e75d570036ef2e4861cee84663bd2
slarnamwong <deknaew_bws@hotmail.com>
2013-09-11 15:54:05 +0700

    แก้ยูไอ

--- GitTest/src/Calculator.java
+++ GitTest/src/Calculator.java
@@ -1,34 +1,1 @@
-import java.util.*;
-
-
-public class Calculator {
-		public static void main(String [] args){
-			
-			
-			Scanner input =new Scanner(System.in);
-			
-			
-			//System.out.println("Push Pull dai la sard");
-			System.out.println("UI branch");
-			
-			System.out.println("NNNum1 : ");
-			int a = input.nextInt();
-			System.out.println("NNNum2 : ");
-			int b = input.nextInt();
-			
-			System.out.println("A+b =  ");
-			System.out.println(plus(a,b));
-			
-			
-		}
-		public static double plus(int x , int y){
-			return x+y;
-		}
-		public static double minus(int x , int y){
-			return x-y;
-		}
-		public static double multiply(int x , int y){
-			return x*y;
-		}
-		
-}
+import java.util.*;public class Calculator {		public static void main(String [] args){									Scanner input =new Scanner(System.in);									//System.out.println("Push Pull dai la sard");			System.out.println("UI branch !");						System.out.println("NNNum1 : ");			int a = input.nextInt();			System.out.println("NNNum2 : ");			int b = input.nextInt();						System.out.println("A+b =  ");			System.out.println(plus(a,b));								}		public static double plus(int x , int y){			return x+y;		}		public static double minus(int x , int y){			return x-y;		}		public static double multiply(int x , int y){			return x*y;		}		}
